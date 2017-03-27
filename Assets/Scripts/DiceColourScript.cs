b087a1a3e49ff98d7413be5e3291ee37afec9ff5
toddlockwood <arkadiusz.lapajski@hotmail.com>
2017-03-27 12:02:03 +0200

    Up2

---
+++ Assets/Scripts/DiceColourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DiceColourScript : MonoBehaviour
+{
+    public GameObject dice;
+    string[] DicesColours = { "Red", "Blue", "Black", "Green", "Grey", "Yellow" };
+    string DiceColour = "";
+
+
+    private void Start()
+    {
+        Coloring();
+    }
+    public void Coloring()
+    {
+        DiceColour = DicesColours[Random.Range(0, DicesColours.Length)];
+        Material DiceMaterial = Resources.Load("Materials/" + DiceColour)as Material;
+        dice.GetComponent<Renderer>().material = DiceMaterial;
+    }
+
+}
