afd746ee6908f27f1f6cd8e6eeb3135977672e3b
toddlockwood <arkadiusz.lapajski@hotmail.com>
2017-03-29 15:48:35 +0200

    up3

---
+++ Assets/Resources/Scripts/DiceColourScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceColourScript : MonoBehaviour
+{
+    public GameObject dice;
+    string[] DicesColours = { "Red", "Blue", "Black", "Green", "Grey", "Yellow" };
+    public string DiceColour = "";
+    
+
+    private void Start()
+    {
+            Coloring();
+    }
+    public void Coloring()
+    {
+        DiceColour = DicesColours[Random.Range(0, DicesColours.Length)];
+        Material DiceMaterial = Resources.Load("Materials/" + DiceColour)as Material;
+        dice.GetComponent<Renderer>().material = DiceMaterial;
+
+        if(DiceColour == "Black")
+        {
+            dice.tag = "Black";
+        }
+        if (DiceColour == "Red")
+        {
+            dice.tag = "Red";
+        }
+        if (DiceColour == "Blue")
+        {
+            dice.tag = "Blue";
+        }
+        if (DiceColour == "Green")
+        {
+            dice.tag = "Green";
+        }
+        if (DiceColour == "Grey")
+        {
+            dice.tag = "Grey";
+        }
+        if (DiceColour == "Yellow")
+        {
+            dice.tag = "Yellow";
+        }
+        
+    }
+
+
+
+}
---
+++ Assets/Resources/Scripts/GridScript.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GridScript : MonoBehaviour
+{
+
+    private int GridHeight = 6;
+    private int GridWidth = 6;
+    public GameObject DicePrefab;
+    GameObject GM;
+    public GameObject[,]Dices = new GameObject[6,6];
+    public int AmountToMatch = 3;
+
+
+	void Start () {
+        MakeArray();
+    
+        
+    }
+    void MakeArray()
+    {
+
+        for (int i = 0; i < GridHeight; i++)
+        {
+
+
+            for (int j = 0; j < GridWidth; j++)
+            {
+                GM = Instantiate(DicePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                GM.transform.parent = gameObject.transform;
+                Dices[i, j] = GM;
+                
+            }
+        }
+    }
+	void CheckOK()
+    {
+        List<GameObject> Dice1List = new List<GameObject>();
+        List<GameObject> Dice2List = new List<GameObject>();
+
+
+        ///Sprawdzanie ciągu
+        for (int i = 0; i<GridHeight;i++)
+        {
+
+            for(int j = 0; j<GridWidth;j++)
+            {
+                
+                if(Dice1List == null)
+                {
+                    Dice1List.Add(Dices[i, j]);
+                    continue;
+                }
+                if (Dice1List[Dice1List.Count-1].CompareTag(Dices[i,j].tag))
+                {
+                    Dice1List.Add(Dices[i, j]);
+
+                    if (Dice1List.Count == 2)
+                    {
+                        Debug.Log("Równe 3!!!!!!!!");
+                    }
+                    continue;
+                }
+                if (Dice1List[Dice1List.Count - 1].CompareTag(Dices[i, j].tag) == false)
+                {
+                    Dice1List.Clear();
+                }
+
+            }
+
+        }
+
+    }
+
+
+    void Update()
+    {
+        CheckOK();
+    }
+}
