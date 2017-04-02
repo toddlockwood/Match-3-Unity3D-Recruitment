270e9019c3d2fa39bebe98e5e00c56eb341772a9
toddlockwood <arkadiusz.lapajski@hotmail.com>
2017-04-02 12:14:19 +0200

    up5

--- Assets/Resources/Scripts/GridScript.cs
+++ Assets/Resources/Scripts/GridScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+using System;
 
 public class GridScript : MonoBehaviour
 {
@@ -41,39 +41,116 @@ public class GridScript : MonoBehaviour
         List<GameObject> Dice2List = new List<GameObject>();
 
 
-        ///Sprawdzanie ciągu
-        for (int i = 0; i<GridHeight;i++)
-        {
 
-            for(int j = 0; j<GridWidth;j++)
+
+        ///Sprawdzanie ciągu
+        for (int i = 0; i < GridHeight; i++)
+        {
+            Dice1List.Clear();
+            for (int j = 0; j < GridWidth; j++)
             {
-                
-                if(Dice1List.Count == 0)
+
+                if (Dice1List.Count == 0)
                 {
                     Dice1List.Add(Dices[i, j]);
                     continue;
                 }
-                if (Dice1List[Dice1List.Count-1].CompareTag(Dices[i,j].tag))
+                if (Dice1List[Dice1List.Count - 1].CompareTag(Dices[i, j].tag))
                 {
+
                     Dice1List.Add(Dices[i, j]);
 
                     if (Dice1List.Count == 3)
                     {
+                        Dices[i, j] = null;
+                        Dices[i - 1, j] = null;
+                        Dices[i - 2, j] = null;
                         Debug.Log("Równe 3!!!!!!!!");
-                        foreach(GameObject GM in Dice1List)
+                        foreach (GameObject GM in Dice1List)
                         {
                             Destroy(GM);
+                            
+
                         }
+                        Dice1List.Clear();
+                    
+
+
                     }
                     continue;
                 }
-                if (Dice1List[Dice1List.Count - 1].CompareTag(Dices[i, j].tag) == false)
+                if ((Dice1List[Dice1List.Count - 1].CompareTag(Dices[i, j].tag) == false))
                 {
                     Dice1List.Clear();
+                    Dice1List.Add(Dices[i, j]);
                 }
 
             }
+        }
 
+            //for (int i = 0; i<GridHeight; i++)
+            //{
+            //    Dice2List.Clear();
+            //    for (int j = 0; j<GridWidth; j++)
+            //    {
+
+            //        if (Dice2List.Count == 0)
+            //        {
+            //            Dice2List.Add(Dices[j, i]);
+            //            continue;
+            //        }
+            //        if (Dice2List[Dice2List.Count - 1].CompareTag(Dices[j, i].tag))
+            //        {
+
+            //            Dice2List.Add(Dices[j, i]);
+
+            //            if (Dice2List.Count == 3)
+            //            {
+            //                Debug.Log("Równe 3!!!!!!!!");
+            //                foreach (GameObject GM in Dice2List)
+            //                {
+            //                    Destroy(GM);
+
+            //                }
+            //                Dice2List.Clear();
+            //            }
+            //            continue;
+            //        }
+            //        if ((Dice2List[Dice2List.Count - 1].CompareTag(Dices[j, i].tag) == false))
+            //        {
+            //            Dice2List.Clear();
+            //            Dice2List.Add(Dices[j, i]);
+            //        }
+
+            //    }
+
+            //}
+
+        }
+    void RefactorDices()
+    {
+        List<GameObject> DiceListRefactor = new List<GameObject>();
+
+        for(int i = 0;i<GridHeight;i++)
+        {
+
+            for( int j = 0;j<GridWidth;j++)
+            {
+                if(Dices[i,j] == null)
+                {
+                    for (int k = i + 2; k < GridHeight; k++)
+                    {
+                        DiceListRefactor.Add(Dices[k, j]);
+
+                        foreach (GameObject GM in DiceListRefactor)
+                        {
+                            Dices[k, j] = Dices[i, j];
+                            
+                        }
+                    }
+
+                }
+            }
         }
 
     }
@@ -81,8 +158,10 @@ public class GridScript : MonoBehaviour
 
     void Update()
     {
-        CheckOK();
 
+
+        CheckOK();
+        RefactorDices();
 
     }
 }
